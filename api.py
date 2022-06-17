6dce80ad23515e9cb3381b688eb138ae342d1f9d
nghia.mt <nghia.mt@citigo.com.vn>
2022-06-17 11:23:32 +0700

    first cm

--- api.py
+++ api.py
@@ -1,24 +1,18 @@
 from flask import Flask, render_template, json, request, jsonify, Response
 import redis
 
+with open("data/20220616-frequency_bougth_together.json", "r") as f:
+    product_id_btgs = json.load(f)
 
 app = Flask(__name__)
 r = redis.Redis(host='localhost', port=6379, db=0)
 
 def get_product_id_from_redis(industry_id, province_id, product_barcode):
-    product_objects = json.loads(r.get(product_barcode))
+    product_objects = product_id_btgs[product_barcode]
     for product_object in product_objects:
         if product_object["industry_id"] == str(industry_id) and product_object["province_id"] == str(province_id):
             return product_object["list_product_id"]
     return ""
-
-@app.route('/load_btg', methods=['GET'])
-def load_btg():
-    with open("data/20220616-frequency_bougth_together.json", "r") as f:
-        product_id_btgs = json.load(f)
-    for product_id_btg in product_id_btgs:
-        r.set(product_id_btg, json.dumps(product_id_btgs[product_id_btg]))
-    return "OK"
 
 @app.route('/frequency_bought_together', methods=['GET'])
 def frequency_bought_together():
@@ -48,5 +42,4 @@ def frequency_bought_together():
         return "Exception: " + str(e)
 
 if __name__ == '__main__':
-    load_btg()
     app.run(host="0.0.0.0", port=5005)
